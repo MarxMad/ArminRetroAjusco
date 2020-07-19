eb1e61f56774dcdfd56b9a8bc043e7e4a3768a42
GamingRetro <rangebote@gmail.com>
2020-07-19 15:45:08 -0500

    nuevo index

--- public/js/app.js
+++ public/js/app.js
@@ -20,3 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+
