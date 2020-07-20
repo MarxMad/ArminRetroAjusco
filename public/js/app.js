675f018147f6b17982fd6cbf64a15f7b8a951f74
GamingRetro <rangebote@gmail.com>
2020-07-20 12:06:38 -0500

    mas juegos

--- public/js/app.js
+++ public/js/app.js
@@ -26,3 +26,13 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+$('.pushpin-demo-nav').each(function() {
+  var $this = $(this);
+  var $target = $('#' + $(this).attr('data-target'));
+  $this.pushpin({
+    top: $target.offset().top,
+    bottom: $target.offset().top + $target.outerHeight() - $this.height()
+  });
+});
+
+
